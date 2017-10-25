a66808978cf648f475b91ae9b80f17ed5f236cc3
Perfor <913661647@qq.com>
2017-10-25 15:08:05 +0800

    1.user cocoapods install thirdpart 2.creat file folder

---
+++ AlarNews/AlarNews/Class/MainClass.swift
@@ -0,0 +1,9 @@
+//
+//  MainClass.swift
+//  AlarNews
+//
+//  Created by Kim on 2017/10/25.
+//  Copyright © 2017年 Brain. All rights reserved.
+//
+
+import Foundation
---
+++ AlarNews/AlarNews/Define/APPInfoDefine.swift
@@ -0,0 +1,9 @@
+//
+//  APPInfoDefine.swift
+//  AlarNews
+//
+//  Created by Kim on 2017/10/25.
+//  Copyright © 2017年 Brain. All rights reserved.
+//
+
+import Foundation
